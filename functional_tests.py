4209eed783f07c0628598fbf2ef799fdd1740cde
Milt Rue <miltonrue03@gmail.com>
2026-07-22 18:22:03 -0500

    Using unittest for functional testing

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,32 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
 
-assert "Congratulations!" in browser.title
-print("OK")
+    def test_can_start_todo_list(self):
+        # Check out this sick to-do app homepage!
+        self.browser.get("http://localhost:8000")
+
+        # The page title/header even mention to-do lists!
+        self.assertIn("To-Do", self.browser.title)
+
+        # Lets add an item
+        self.fail("Finish the test!")
+
+        # We type "Eat chicken nuggets" into a text box
+
+        # We hit enter, and the page updates
+        # The list now reads "1: Eat chicken nuggets"
+
+        # We can add another item, "Eat french fries"
+
+        # Another page update, showing both items
+
+        # We're happy the app works, so we log off
+
+if __name__ == "__main__":
+    unittest.main()
